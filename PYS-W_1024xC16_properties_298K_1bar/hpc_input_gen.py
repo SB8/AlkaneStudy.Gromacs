22ab7b73986abef53f6621e159a7f99870ea8c82
Stephen <stephen.burrows@hotmail.com>
2019-03-13 19:58:07 +0000

    Readded PYS-W property simulations with corrected ffbonded.itp

--- PYS-W_1024xC16_properties_298K_1bar/hpc_input_gen.py
+++ PYS-W_1024xC16_properties_298K_1bar/hpc_input_gen.py
@@ -11,8 +11,8 @@ from sim_class import SimGromacs, finalize_simulation
 outputDir = os.getcwd()
 
 shellName = 'run_gromacs.sh'
-groStart = '2048xC7_UA_start.gro'
-hpcHeader = 'MMM_header_2016-3.sh' 
+currentCoords = '1024xC16_UA_start.gro'
+hpcHeader = os.path.join(gmxModDir, 'MMM_header_2016-3.sh')
 mdrunCmd = 'gerun mdrun_mpi'
 
 # Strings to replace in shell header
@@ -36,7 +36,7 @@ newSim = SimGromacs([mdpFF, mdp.EM], shellFile,
 			mdrun=mdrunCmd,
 			suffix='EM', 
 			traj='trr',
-			coords=groStart)
+			coords=currentCoords)
 # This stores the filename of the current coordinate file (.gro)
 currentCoords = newSim.coordsOut
 # Write EM to file
@@ -48,7 +48,7 @@ newSim = SimGromacs([mdpFF, mdp.NPT_eq], shellFile,
 			suffix='NPT_eq', 
 			coords=currentCoords)
 currentCoords = newSim.coordsOut
-newSim.set_param('nsteps', 1000000)
+newSim.set_param('nsteps', 2000000)
 finalize_simulation(newSim, shellFile, outputDir)
 
 # NPT production run
@@ -57,26 +57,19 @@ newSim = SimGromacs([mdpFF, mdp.NPT], shellFile,
 			suffix='NPT_sim', 
 			coords=currentCoords)
 currentCoords = newSim.coordsOut
-newSim.set_param('nsteps', 1000000) 
+newSim.set_param('nsteps', 5000000) 
 finalize_simulation(newSim, shellFile, outputDir)
 
-# cos-acceleration (viscosity) sim
-base_acc = 0.01
-base_nsteps = 4000000
-f = 2**0.5
-extrapoints = [4,2] # Below and above
-acc = base_acc/f**extrapoints[0]
+# Call box_resize.py
+shellFile.write('\nsleep 1\n')
+shellFile.write('python3 box_resize.py gmx '+currentCoords+' 3 gro_interface_start.gro\nsleep 1\n')
+currentCoords = 'gro_interface_start.gro'
 
-for i in range(1+sum(extrapoints)):
-	newSim = SimGromacs([mdpFF, mdp.NPT], shellFile, 
-				mdrun=mdrunCmd,
-				suffix='NPT_cosacc_'+'{:.4f}'.format(acc),
-				coords=currentCoords)
-	currentCoords = newSim.coordsOut
-	newSim.set_param('nsteps', int((base_nsteps+1)*base_acc/acc/100000)*100000)
-	newSim.set_param('nstcalcenergy', 10) 
-	newSim.set_param('nstenergy', 100) 
-	newSim.set_param('cos-acceleration', '{:.6f}'.format(acc))
-	newSim.set_param('nstxout-compressed', 10000) # Trajectory less important
-	finalize_simulation(newSim, shellFile, outputDir)
-	acc *= f
+# NVT simulation with interface (for surface tension)
+newSim = SimGromacs([mdpFF, mdp.NVT_interface_PMEVdW], shellFile, 
+			mdrun=mdrunCmd,
+			suffix='NVT_interface', 
+			coords=currentCoords)
+currentCoords = newSim.coordsOut
+newSim.set_param('nsteps', 5000000) 
+finalize_simulation(newSim, shellFile, outputDir)
--- gromacs_hpc_input_gen/MMM_header_2016-3.sh
+++ gromacs_hpc_input_gen/MMM_header_2016-3.sh
@@ -11,6 +11,7 @@
 module unload compilers/intel/2018/update3
 module unload mpi/intel/2018/update3/intel
 
+module load python3
 module load compilers/intel/2017/update1
 module load mpi/intel/2017/update1/intel
 module load gromacs/2016.3/intel-2017-update1
