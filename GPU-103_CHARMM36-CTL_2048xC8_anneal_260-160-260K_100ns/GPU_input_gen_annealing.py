098a815e85ce6c807e82f875f0497a3bd580e50e
Stephen <stephen.burrows@hotmail.com>
2019-03-20 14:17:17 +0000

    Added GPU-103, CHARMM36 C8 properties + annealing

--- GPU-103_CHARMM36-CTL_2048xC8_anneal_260-160-260K_100ns/GPU_input_gen_annealing.py
+++ GPU-103_CHARMM36-CTL_2048xC8_anneal_260-160-260K_100ns/GPU_input_gen_annealing.py
@@ -11,7 +11,7 @@ from sim_class import SimGromacs, finalize_simulation
 outputDir = os.getcwd()
 
 shellName = 'run_gromacs_GPU.sh'
-currentCoords = '2048xC8-AA.gro' 
+currentCoords = '2048xC8-AA_5nsEq-CHARMM36.gro' 
 mdrunCmd = 'gmx mdrun'
 
 # Set force field parameters
@@ -24,7 +24,7 @@ mdp_anneal['annealing-time'] = '0 50000' # ps
 mdp_anneal['nsteps'] = '50000000' # 50ns - 1fs time step!
 mdp_anneal['nstxout-compressed'] = '50000' # 1 frame = 0.1K, 1000 frames each half
 
-shellFile = open(os.path.join(outputDir,shellName), 'w')
+shellFile = open(os.path.join(outputDir,shellName), 'w', newline='\n')
 
 # Simulated annealing, decreasing T
 T_m = 210 # Rounded down
--- GPU-103_CHARMM36-CTL_2048xC8_anneal_260-160-260K_100ns/run_gromacs_GPU.sh
+++ GPU-103_CHARMM36-CTL_2048xC8_anneal_260-160-260K_100ns/run_gromacs_GPU.sh
@@ -1,5 +1,5 @@
 
-gmx grompp -f mdp_NPT_anneal_260-160K.mdp -c 2048xC8-AA.gro -p topol.top -o tpr_NPT_anneal_260-160K.tpr
+gmx grompp -f mdp_NPT_anneal_260-160K.mdp -c 2048xC8-AA_5nsEq-CHARMM36.gro -p topol.top -o tpr_NPT_anneal_260-160K.tpr
 gmx mdrun -s tpr_NPT_anneal_260-160K.tpr  -c gro_NPT_anneal_260-160K.gro -g log_NPT_anneal_260-160K.log -e edr_NPT_anneal_260-160K.edr -x xtc_NPT_anneal_260-160K.xtc
 
 gmx grompp -f mdp_NPT_anneal_160-260K.mdp -c gro_NPT_anneal_260-160K.gro -p topol.top -o tpr_NPT_anneal_160-260K.tpr
