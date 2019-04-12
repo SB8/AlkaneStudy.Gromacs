0c74da7cf6fae9d459ed161fab9450aad37af9e8
Stephen Burrows <stephen.burrows@hotmail.com>
2019-04-12 14:55:05 +0100

    Added Sun9-6 C8 input files

---
+++ Sun9-6_2048xC8_anneal_260-160-260K_100ns/run_gromacs_heating.sh
@@ -0,0 +1,20 @@
+#$ -S /bin/bash
+#$ -l h_rt=48:00:00
+#$ -l mem=4G
+#$ -l tmpfs=10G
+#$ -N GROMACS
+#$ -pe mpi 96
+#$ -P Gold
+#$ -A QMUL_SMOUKOV
+#$ -cwd
+
+module unload compilers/intel/2018/update3
+module unload mpi/intel/2018/update3/intel
+
+module load python3
+module load compilers/intel/2017/update1
+module load mpi/intel/2017/update1/intel
+module load gromacs/2016.3/intel-2017-update1
+
+gmx grompp -f mdp_NPT_anneal_160-260K.mdp -c gro_NPT_anneal_260-160K.gro -p topol.top -o tpr_NPT_anneal_160-260K.tpr
+gerun mdrun_mpi -s tpr_NPT_anneal_160-260K.tpr -c gro_NPT_anneal_160-260K.gro -g log_NPT_anneal_160-260K.log -e edr_NPT_anneal_160-260K.edr -x xtc_NPT_anneal_160-260K.xtc -table table6-9.xvg
---
+++ Sun9-6_2048xC8_properties_298K_1bar/run_gromacs.sh
@@ -0,0 +1,30 @@
+#$ -S /bin/bash
+#$ -l h_rt=48:00:00
+#$ -l mem=4G
+#$ -l tmpfs=10G
+#$ -N GROMACS
+#$ -pe mpi 96
+#$ -P Gold
+#$ -A QMUL_SMOUKOV
+#$ -cwd
+
+module unload compilers/intel/2018/update3
+module unload mpi/intel/2018/update3/intel
+
+module load python3
+module load compilers/intel/2017/update1
+module load mpi/intel/2017/update1/intel
+module load gromacs/2016.3/intel-2017-update1
+
+gmx grompp -f mdp_NPT_eq.mdp -c 2048xC8-AA_5nsEq-CHARMM36.gro -p topol.top -o tpr_NPT_eq.tpr
+gerun mdrun_mpi -s tpr_NPT_eq.tpr -c gro_NPT_eq.gro -g log_NPT_eq.log -e edr_NPT_eq.edr -x xtc_NPT_eq.xtc -table table6-9.xvg
+
+gmx grompp -f mdp_NPT_sim.mdp -c gro_NPT_eq.gro -p topol.top -o tpr_NPT_sim.tpr
+gerun mdrun_mpi -s tpr_NPT_sim.tpr -c gro_NPT_sim.gro -g log_NPT_sim.log -e edr_NPT_sim.edr -x xtc_NPT_sim.xtc -table table6-9.xvg
+
+sleep 1
+python3 box_resize.py gmx NPT_sim 3 gro_interface_start.gro
+sleep 1
+
+gmx grompp -f mdp_NVT_interface.mdp -c gro_interface_start.gro -p topol.top -o tpr_NVT_interface.tpr
+gerun mdrun_mpi -s tpr_NVT_interface.tpr -c gro_NVT_interface.gro -g log_NVT_interface.log -e edr_NVT_interface.edr -x xtc_NVT_interface.xtc -table table6-9.xvg
